472b2505de9aa947e382c39b8fe996b3abd0899e
Sameer <samirlama509@gmail.com>
2021-01-26 16:49:27 +0545

    montage displayed.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,15 @@ class PagesController < ApplicationController
 
 
   def index
-
+    path = Dir.glob('public/uploads/5/muvy*')
+    if path.present?
+      @image_url = "/" + path.first.split('/').pop(3).join('/')
+    end
   end
 
   def create
-    muvy = Muvy::Media.new(params[:url][:youtube], { tmp_dir: 'public/uploads/5/' }).run
+    muvy = Muvy::Media.new(params[:url][:youtube], { tmp_dir: 'public/uploads/5/', path: 'public/uploads/5/' }).run
+    redirect_to root_path
   end
 
   # def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
-  resources :pages, only: [:create]
+  resources :pages, only: %i[create]
 end
